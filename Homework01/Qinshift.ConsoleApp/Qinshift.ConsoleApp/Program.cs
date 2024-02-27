5ac98f8f348b99abb35e3f9e4c08f84bb869d217
Code <code@sedc.mk>
2024-02-27 19:53:44 +0100

    add new homework repository

---
+++ Homework01/Qinshift.ConsoleApp/Qinshift.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//WriteLine stava sekoja poraka vo svoj red, a Write gi redi edno po drugo
+//ReadLine go zapira kodot i ti dozvoluva da pises neso
+Console.WriteLine("    *     ");
+Console.WriteLine("    *     ");
+Console.WriteLine(" *  *  *  ");
+Console.WriteLine("*   *   * ");
+Console.WriteLine("    *     ");
+Console.WriteLine("    *     ");
+Console.WriteLine(" *     *    ");
+Console.WriteLine("*       * ");
+
+
