d969c3225b91cc8d2ac4a8827155a8dce95511b5
Andrej Jankovikj <152323152+AndrejJankovikj@users.noreply.github.com>
2024-03-08 02:19:35 +0100

    homework task 2 7.3.2024

---
+++ Homework03 7.3.2024/Solution1/Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Hello, World!");
+
+
+
+
+//DateTime currentDate= DateTime.Today;
+//Console.WriteLine(currentDate);
+
+//Console.Write("Enter your date of birth(mm/dd/yyyy)  ");
+//DateTime yourBirthday = DateTime.Parse(Console.ReadLine());
+//Console.WriteLine(yourBirthday);
+//TimeSpan value = currentDate.Subtract(yourBirthday);
+//Console.WriteLine(value);
+
+//DateTime birthdayDay = currentDate.AddDays(-yourBirthday.Day);
+//DateTime birthdayMonth=currentDate.AddMonths(-yourBirthday.Month);
+//DateTime birthdayYear=currentDate.AddYears(-yourBirthday.Year);
+//string birthdayDayString = yourBirthday.ToString("mm/dd/yyyy");
+
+//static DateTime birthdayCalculator(DateTime yourBirthday,DateTime currentDate)
+//{
+//    DateTime yourBirthdayResult = currentDate - yourBirthday
+
+//    return yourBirthdayResult;
+//}
+//Console.WriteLine();
+
---
+++ Homework03 7.3.2024/Solution1/Homework2 8.3.20204/Program.cs
@@ -0,0 +1,65 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+//Console.WriteLine("Hello, World!");
+//Write a method that formats a given date and time as a string in a specific format.
+//Get the needed inputs from screen.
+//Allowed formats: "MM/dd/yyyy", "MM/dd/yyyy hh:mm:ss", "dddd, dd MMMM yyyy HH:mm:ss", "MM.dd.yyyy"
+
+//Bonus: Create separate method that validates whether the entered format is allowed
+
+
+
+
+//Console.Write("Enter your date of birth(mm/dd/yyyy hh:mm:ss):  ");
+//DateTime dateInput = DateTime.Parse(Console.ReadLine());
+//static void printInputDate(DateTime dateInput)
+//{
+//string theDate = dateInput.ToString();
+//    string printTheDate = "The date your wrote is: " + theDate;
+////Console.WriteLine(theDate);
+//    return printTheDate;
+
+//}
+//string inputDate = printInputDate(DateTime dateInput);
+
+
+
+
+static void DateFormat()
+{
+
+DateTime theDate = new DateTime(1994, 5, 24, 7, 20, 30);
+
+
+Console.Write("Input the format you would like the date to be printed in(\n\"MM/dd/yyyy\"\n\"MM/dd/yyyy hh:mm:ss\"\n \"dddd, dd MMMM yyyy HH:mm:ss\"\n \"MM.dd.yyyy\"):  \n");
+string theFormatInput = Console.ReadLine();
+
+
+if(theFormatInput== "mm/dd/yyyy")
+{
+    string dateFormat = theDate.ToString("mm/dd/yyyy");
+    Console.WriteLine(dateFormat);
+}
+else if(theFormatInput== "MM/dd/yyyy hh:mm:ss")
+{
+    string dateFormat = theDate.ToString("mm/dd/yyyy hh:mm:ss");
+    Console.WriteLine(dateFormat);
+
+}
+else if(theFormatInput== "dddd, dd MMMM yyyy HH:mm: ss") {
+    string dateFormat = theDate.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+    Console.WriteLine(dateFormat);
+}
+else if(theFormatInput== "MM.dd.yyyy")
+{
+    string dateFormat = theDate.ToString("MM.dd.yyyy");
+    Console.WriteLine(dateFormat);
+}
+    else
+    {
+        Console.WriteLine("Invalid format input!");
+    }
+}
+DateFormat();
