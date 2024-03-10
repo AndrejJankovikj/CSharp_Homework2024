d03261f6179057819b4b5a3c06203966fbcc3cf0
Andrej Jankovikj <152323152+AndrejJankovikj@users.noreply.github.com>
2024-03-11 00:27:10 +0100

    Commit na domasnoto od 9.3.2024

--- Homework4 9.3.2024/Homework1/Homework1/Program.cs
+++ Homework4 9.3.2024/Homework1/Homework1/Program.cs
@@ -1,9 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Homework1;
-using static System.Net.Mime.MediaTypeNames;
-using System.Reflection.Metadata;
-using System.Reflection;
-using System;
+
 //Make a class Driver. Add properties: Name, Skill
 //Make a class Car. Add properties: Model, Speed and Driver
 //Make a method of the Car class called : CalculateSpeed() that takes a driver object and calculates the skill multiplied by the speed of the car and return it as a result.
@@ -38,3 +34,77 @@ using System;
 //BONUS 2: Make the Output message say what was the model of the car that won, what speed was it going and which driver was driving it.
 
 //BONUS 3: Implement a Race Again Feature where you ask the user if he wants to race again and repeat the racing function.
+
+using Homework01.Driver;
+    using Homework01.Car;
+
+Driver ilija = new Driver("Ilija", 7);
+Driver ivan = new Driver("Ivan", 3);
+Driver ivo = new Driver("Ivo", 5);
+Driver danilo = new Driver("Danilo", 9);
+
+Driver[] availableDrivers = { ilija, ivan, ivo, danilo };
+
+Console.WriteLine("Please choose your driver for the first car: (Ilija/Ivan/Ivo/Danilo)");
+string firstDriver = Console.ReadLine();
+Console.WriteLine("Please choose your driver for the second car: (Ilija/Ivan/Ivo/Danilo)");
+string secondDriver = Console.ReadLine();
+
+Driver? selectedFirstDriver = null;
+Driver? selectedSecondDriver = null;
+
+foreach (Driver driver in availableDrivers)
+{
+    if (firstDriver?.ToLower() == driver.Name.ToLower())
+    {
+        selectedFirstDriver = driver;
+    }
+    else
+    if (secondDriver?.ToLower() == driver.Name.ToLower())
+    {
+        selectedSecondDriver = driver;
+    }
+
+}
+
+
+Car hyundai = new Car("Hyundai", 3);
+Car mazda = new Car("Mazda", 5);
+Car ferrari = new Car("Ferrari", 6);
+Car porsche = new Car("Porsche", 9);
+
+Car[] availableCars = { hyundai, mazda, ferrari, porsche };
+
+Console.WriteLine("Please choose your car for the first car: (Hyundai/Mazda/Ferrari/Porsche)");
+string firstCarModel = Console.ReadLine();
+Console.WriteLine("Please choose your car for the second car: (Hyundai/Mazda/Ferrari/Porsche)");
+string secondCarModel = Console.ReadLine();
+
+Car? selectedFirstCar = null;
+Car? selectedSecondCar = null;
+
+foreach (Car car in availableCars)
+{
+    if (firstCarModel?.ToLower() == car.Model.ToLower())
+    {
+        selectedFirstCar = car;
+    }
+    else
+    if (secondCarModel?.ToLower() == car.Model.ToLower())
+    {
+        selectedSecondCar = car;
+    }
+}
+
+
+if (selectedFirstCar == null || selectedSecondCar == null || selectedFirstDriver == null || selectedSecondDriver == null)
+{
+    Console.WriteLine("You have selected invalid car or driver or duplicate car or driver!!");
+    return;
+}
+
+
+selectedFirstCar!.Driver = selectedFirstDriver;
+selectedSecondCar!.Driver = selectedSecondDriver;
+
+Car.RaceCars(selectedFirstCar, selectedSecondCar);
