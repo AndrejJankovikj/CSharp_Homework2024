4c192d910fed309e1a2cb03da81458353d8c1ae6
Andrej Jankovikj <152323152+AndrejJankovikj@users.noreply.github.com>
2024-03-10 01:29:20 +0100

    Commit site domasni

---
+++ Homework03 7.3.2024/Solution1/Homework3/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Given a string, create a method to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+//Examples:
+//ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+//ReverseCase("MANY THANKS") ➞ "many thanks"
+
+//ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+
+static string ReverseCase()
+{
+
+Console.Write("Write what you want reversed: ");
+string inputValue = Console.ReadLine();
+    //char[] newCharArray = { };
+    char[] inputChars = inputValue.ToCharArray();
+    char[] newCharArray = new char[inputChars.Length];
+
+   for (int i = 0; i < inputChars.Length; i++)
+    {
+        if (Char.IsLower(inputChars[i]))
+        {
+            newCharArray[i] = char.ToUpper(inputChars[i]);
+        }
+        else if (Char.IsUpper(inputChars[i]))
+        {
+            newCharArray[i] = char.ToLower(inputChars[i]);
+        }
+        else
+        {
+            newCharArray[i] = inputChars[i];
+        }
+    }
+    string reversedString = new string(newCharArray);
+    return $"The reversed string looks like this: {reversedString}";
+   
+}
+string reversed = ReverseCase();
+Console.WriteLine(reversed);
---
+++ Homework03 7.3.2024/Solution1/Homework4/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Mail;
+
+Console.WriteLine("Hello, World!");
+//Create a method that accepts a string, checks if it's a valid email address and returns either true or false, depending on the evaluation.
+
+//The string must contain an @ character.
+//The string must contain a . character.
+//The @ must have at least one character in front of it.
+//ex. "e@mail.com" is valid, while "@email.com" is invalid.
+//The . and the @ must be in the appropriate places.
+//ex. "hello.email@com" is invalid while "john.smith@email.com" is valid.
+//If the string passes these tests, it's considered a valid email address.
+
+//Examples:
+//ValidateEmail("@gmail.com") ➞ false
+
+//ValidateEmail("hello.gmail@com") ➞ false
+
+//ValidateEmail("gmail") ➞ false
+
+//ValidateEmail("hello@gmail") ➞ false
+
+//ValidateEmail("hello@gmail.com") ➞ true
+
+
+    Console.Write("Enter the email you want to validate: ");
+    string inputEmail = Console.ReadLine();
+static bool EmailValidation(string email)
+{
+    if (!email.Contains("@") || !email.Contains(".")) { 
+    return false;
+} 
+    int atIndex = email.IndexOf("@");
+    if (atIndex == 0)
+    {
+        return false;
+    }
+    int dotIndex = email.LastIndexOf(".");
+    if (dotIndex < atIndex + 2 || dotIndex == email.Length - 1)
+        return false;
+    if (email.LastIndexOf(".") == email.Length - 1)
+    {
+        return false;
+    }
+    return true;
+
+}
+string emailValidation=$"The input email after evaluation has returned: {EmailValidation(inputEmail)}";
+Console.WriteLine(emailValidation);
+
+
+
