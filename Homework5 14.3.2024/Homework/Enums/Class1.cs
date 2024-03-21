6fd817ebde9aa76bc466cabd093e80b6d16149f8
Andrej Jankovikj <152323152+AndrejJankovikj@users.noreply.github.com>
2024-03-21 15:38:45 +0100

    Commit homework 14.3.2024

---
+++ Homework5 14.3.2024/Homework/Enums/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework.Enums
+{
+    public enum Role
+    {
+        Ceo = 1,
+        Manager = 2,
+        Salesman = 3,
+        Other = 4,
+    }
+}
---
+++ Homework5 14.3.2024/Homework/Program.cs
@@ -0,0 +1,19 @@
+﻿using Homework.Classes;
+Console.ForegroundColor=ConsoleColor.DarkCyan;
+
+Manager manager1 = new Manager("Mario", "Gulicovski", "ESZ");
+Manager manager2 = new Manager("Martin", "Mitikj", "EBR");
+
+Employee[] company = new Employee[5]
+{
+    new Contractor("Ivan","Apostolovski", 6,200, manager1),
+    new Contractor("Danilo","Borozan",2,400, manager2),
+    manager1,
+    manager2,
+    new SalesPerson("Ilija", "Mitev")
+};
+
+Ceo ceo = new Ceo("Andrej", "Jankovikj", company, 10000);
+ceo.PrintInfo();
+ceo.PrintEmployees();
+ceo.GetSalary();
